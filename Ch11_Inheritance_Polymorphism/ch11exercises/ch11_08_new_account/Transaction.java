ee2f327e5a0280272265cd5a6528bbe9b46829c4
khal <peoul@msn.com>
2020-12-11 06:40:09 -0500

    MyStack Class that stores objects in ArrayList

---
+++ Ch11_Inheritance_Polymorphism/ch11exercises/ch11_08_new_account/Transaction.java
@@ -0,0 +1,12 @@
+package ch11exercises.ch11_08_new_account;
+
+/**
+ * -date: java.util.Date -type: char
+ * -amount: double -balance: double -description: String
+ *
+ * +Transaction(type: char, amount: double, balance: double, description: String)
+ *
+ */
+public class Transaction
+{
+}
---
+++ Ch11_Inheritance_Polymorphism/ch11notes/newnotes/arraylist/MyStack.java
@@ -0,0 +1,81 @@
+package ch11notes.newnotes.arraylist;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ * -list: ArrayList<Object>
+ * +isEmpty(): boolean
+ * +getSize(): int
+ * +peek(): Object
+ * +pop(): Object
+ * +push(o: Object): void
+ */
+
+//We've made a MyStack Class before, this one implements it using ArrayList instead of Arrays
+public class MyStack
+{
+    private ArrayList<Object> list;
+
+    public MyStack()
+    {
+        list = new ArrayList<>();       //init ArrayList
+    }
+
+    public boolean isEmpty()
+    {
+        return (list.size() == 0);
+    }
+
+    public int getSize()
+    {
+        return list.size();
+    }
+
+    //returns but doesn't remove the last obj/element
+    public Object peek()
+    {
+        return (list.get(list.size() - 1));
+    }
+
+    //returns and removes the last obj/element
+    public Object pop()
+    {
+        Object o = peek();
+        list.remove((list.size() - 1));
+        return o;
+    }
+
+    public void push(Object o)
+    {
+        list.add(o);
+    }
+
+    @Override
+    public String toString()
+    {
+        return //super.toString() +
+               "\nMyStack{" +
+               "list=" + list +
+               '}';
+    }
+
+    public static void main(String[] args)
+    {
+        Object date = new Date();
+        Object msg = new String("Hello World");
+
+        Object myStack = new MyStack();
+        ((MyStack) myStack).push(date);
+        ((MyStack) myStack).push(msg);
+
+        System.out.println(myStack.toString());
+
+        System.out.println("Popping MyStack...");
+        ((MyStack) myStack).pop();
+
+        System.out.println(myStack.toString());
+        System.out.println(((MyStack) myStack).getSize());
+
+    }
+}
