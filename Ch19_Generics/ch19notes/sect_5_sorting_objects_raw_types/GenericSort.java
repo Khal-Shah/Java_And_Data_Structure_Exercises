0abbd7e23f5cd68492df95ba79d3308675c2dd41
khal <peoul@msn.com>
2021-06-26 01:25:52 -0400

    Generics: sorting array of objects and raw types

---
+++ Ch19_Generics/ch19notes/sect_5_sorting_objects_raw_types/GenericSort.java
@@ -0,0 +1,77 @@
+package ch19notes.sect_5_sorting_objects_raw_types;
+
+public class GenericSort
+{
+    public static void main(String[] args)
+    {
+        //Integer array
+        Integer[] intArray = {2, 4, 3};
+
+        // Create a Double array
+        Double[] doubleArray = {3.4, 1.3, -22.1};
+
+        // Create a Character array
+        Character[] charArray = {'a', 'J', 'r'};
+
+        // Create a String array
+        String[] stringArray = {"Tom", "Susan", "Kim"};
+
+        //sort the arrays using our generic sorting method
+        sort(intArray);
+        sort(doubleArray);
+        sort(charArray);
+        sort(stringArray);
+
+        // Display the sorted arrays
+        System.out.print("Sorted Integer objects: ");
+        printList(intArray);
+
+        System.out.print("Sorted Double objects: ");
+        printList(doubleArray);
+
+        System.out.print("Sorted Character objects: ");
+        printList(charArray);
+
+        System.out.print("Sorted String objects: ");
+        printList(stringArray);
+    }
+
+    //objects are instances of comparable interface
+    public static <E extends Comparable<E>> void sort(E[] list)
+    {
+        E currentMin;
+        int currentMinIndex;
+
+        //use compareTo method to compare each object
+        for(int i = 0; i < list.length - 1; i++)
+        {
+            currentMin = list[i];
+            currentMinIndex = i;
+
+            for(int j = i + 1; j < list.length; j++)
+            {
+                //if current element is greater than any of the e to the right, make the right e current e
+                if(currentMin.compareTo(list[j]) > 0)
+                {
+                    currentMin = list[j];
+                    currentMinIndex = j;
+                }
+            }
+
+            if(currentMinIndex != i)
+            {
+                list[currentMinIndex] = list[i];    //put bigger e where we found current e
+                list[i] = currentMin;
+            }
+        }
+    }
+
+    public static void printList(Object[] list)
+    {
+        for(Object obj: list)
+        {
+            System.out.printf("%10s", obj);
+        }
+        System.out.println();
+    }
+}
---
+++ Ch19_Generics/ch19notes/sect_5_sorting_objects_raw_types/RawTypes_BackwardCompatibility.java
@@ -0,0 +1,28 @@
+package ch19notes.sect_5_sorting_objects_raw_types;
+
+public class RawTypes_BackwardCompatibility
+{
+    //max method with no generics (raw types)
+    public static Comparable maxNoGeneric(Comparable o1, Comparable o2)      //o1 and o2 here are raw type declarations
+    {
+        return ((o1.compareTo(o2) > 0)? o1 : o2);
+    }
+
+    //.max(23, "welcome") would be a runtime error
+
+    //Now writing the same max method with generic type (better)
+    public static <E extends Comparable<E>> E max(E o1, E o2)
+    {
+        return ((o1.compareTo(o2) > 0)? o1 : o2);
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println(RawTypes_BackwardCompatibility.maxNoGeneric(23, "welcome"));     //runtime error (noGeneric)
+
+//        System.out.println(RawTypes_BackwardCompatibility.max(23, "welcome"));      //compile-time error (generic)
+
+        System.out.print(RawTypes_BackwardCompatibility.maxNoGeneric(20, 22));     //works because both same type
+
+    }
+}
