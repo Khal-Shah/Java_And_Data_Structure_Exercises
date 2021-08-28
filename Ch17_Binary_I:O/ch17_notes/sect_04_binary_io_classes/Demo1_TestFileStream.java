0964466f5aa97fe1f98aa5da3e898c57206c905a
khal <peoul@msn.com>
2021-08-28 17:50:08 -0400

    Ch17 (Binary I/O) - Binary I/O Classes to read/write binary data.

---
+++ Ch17_Binary_I:O/ch17_notes/sect_04_binary_io_classes/Demo1_TestFileStream.java
@@ -0,0 +1,37 @@
+package ch17_notes.sect_04_binary_io_classes;
+
+import java.io.*;
+
+/**
+ * Sect 04: using binary I/O to write 10 byte values from 1 to 10 and then reading it back (file name temp.dat)
+ */
+
+public class Demo1_TestFileStream
+{
+    public static void main(String[] args) throws IOException
+    {
+        //create file
+        File file = new File("Ch17_Binary_I:O/ch17_notes/sect_04_binary_io_classes/temp.txt");
+
+        //create output stream (to write) to the file
+        FileOutputStream writer = new FileOutputStream(file);
+
+        //now write
+        for(int i = 1; i < 11; i++)
+        {
+            writer.write(i);        //same as .write((byte) i)
+        }
+        writer.close();
+
+        //create input stream (read the file we just created and wrote to)
+        FileInputStream reader = new FileInputStream("Ch17_Binary_I:O/ch17_notes/sect_04_binary_io_classes/temp.txt");
+        int value;
+
+        //input value = -1 signifies end of file
+        while((value = reader.read()) != -1)    //
+        {
+            System.out.println(value);
+        }
+        reader.close();
+    }
+}
---
+++ Ch17_Binary_I:O/ch17_notes/sect_04_binary_io_classes/Demo2_TestDataStream.java
@@ -0,0 +1,53 @@
+package ch17_notes.sect_04_binary_io_classes;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Demo2_TestDataStream
+{
+
+    public static void main(String[] args) throws IOException
+    {
+        //create file (notice we have to use FileOutputStream and not File to be able to use DataOutStream
+        FileOutputStream file = new FileOutputStream("Ch17_Binary_I:O/ch17_notes/sect_04_binary_io_classes/grades.dat");
+
+        //data output
+        DataOutputStream output = new DataOutputStream(file);
+        Map<String, Double> grades = new HashMap<>();
+        grades.put("Moe", 82.0);
+        grades.put("Annie", 91.5);
+        grades.put("Joe", 66.6);
+        grades.put("Mark", 78.0);
+        grades.put("Lenny", 84.5);
+
+        output.writeUTF("Moe");
+        output.writeDouble(grades.get("Moe"));
+
+        output.writeUTF("Annie");
+        output.writeDouble(grades.get("Annie"));
+
+        output.writeUTF("Joe");
+        output.writeDouble(grades.get("Joe"));
+
+        output.writeUTF("Mark");
+        output.writeDouble(grades.get("Mark"));
+
+
+        //Now read using DataInputStream
+        DataInputStream input = new DataInputStream(new FileInputStream("Ch17_Binary_I:O/ch17_notes/sect_04_binary_io_classes/grades.dat"));
+
+       while(true)
+       {
+           try
+           {
+               System.out.println(input.readUTF() + ": " + input.readDouble());
+           }
+           catch(EOFException eofException)
+           {
+               System.out.println("All data has been read already!");
+               break;
+           }
+       }
+    }
+}
