f01bbadfd13a5151adbd4c6732c3e5bc7bfb08e1
khal <peoul@msn.com>
2021-09-15 20:35:33 -0400

    Section 3: Bubble Sort (Test Demo)

---
+++ Ch23_Sorting/ch23_notes/sect_3_bubble_sort/Demo_0_Bubble.java
@@ -0,0 +1,33 @@
+package Ch23_Sorting.ch23_notes.sect_3_bubble_sort;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class Demo_0_Bubble
+{
+    @Test
+    public void testBubbleSort()
+    {
+        int[] list = {2, 9, 5, 4, 8, 1};
+        boolean needNextPass = true;
+
+            for(int i = 1; i < list.length && needNextPass; i++)
+            {
+                needNextPass = false;
+                for(int j = 0; j < list.length - i; j++)
+                {
+                    if(list[j] > list[j + 1])
+                    {
+                        //swap
+                        int next = list[j + 1];
+                        list[j + 1] = list[j];
+                        list[j] = next;
+                        needNextPass = true;
+                    }
+                }
+            }
+
+        System.out.println(Arrays.toString(list));
+    }
+}
